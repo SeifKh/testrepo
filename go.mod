5b6333e25b76771bb8d874c3b888eb94ac413e00
sek <seif.khalifa@opensynergy.com>
2022-03-30 19:01:00 +0200

    blah

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SeifKh/testrepo
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "log"
+
+    "github.com/zalando/go-keyring"
+)
+
+func main() {
+    service := "my-app"
+    user := "anon"
+    password := "secret"
+
+    // set password
+    err := keyring.Set(service, user, password)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // get password
+    secret, err := keyring.Get(service, user)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Println(secret)
+}
+
